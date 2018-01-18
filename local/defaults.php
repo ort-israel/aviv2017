417fb6d3a32de04dc640bdae3b7499509df2eb3b
Ort Moodle developer <MoodleDev@ort.org.il>
2018-01-18 11:20:57 +0200

    local/defaults.php (new)

---
+++ local/defaults.php
@@ -0,0 +1,4 @@
+<?php
+$defaults['assignsubmission_file']['maxbytes'] = '512M'; // for plugins
+$defaults['moodle']['autolang'] = '0';     // for core settings
+$defaults['moodle']['grade_includescalesinaggregation'] = '0';     // for core settings
