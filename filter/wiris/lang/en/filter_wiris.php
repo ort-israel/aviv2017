f0b6693a7f31cbd73800266ba55ca63aa9ef0f0e
Ort Moodle developer <MoodleDev@ort.org.il>
2018-01-29 09:26:36 +0200

    filter/wiris (new)

---
+++ filter/wiris/lang/en/filter_wiris.php
@@ -0,0 +1,101 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+$string['accessproviderenabled'] = 'Access control';
+$string['accessproviderenabled_help'] = 'If enabled ony authenticated users can access WIRIS services.';
+$string['alloweditorpluginactive'] = 'WIRIS Editor always active';
+$string['alloweditorpluginactive_help'] = 'WIRIS Editor plugin would be enabled regardless of WIRIS filter is disabled at course or activity level. By default if WIRIS filter is disabled in a course/activity, WIRIS Editor is also disabled in the same course/activity.';
+$string['area'] = 'area';
+$string['arenotinstalled'] = 'are not installed. ';
+$string['atto'] = 'Atto';
+$string['button1'] = 'WIRIS plugin for tests';
+$string['button2'] = 'WIRIS quizzes tests';
+$string['cachedef_images'] = 'WIRIS Plugin images';
+$string['cachedef_formulas'] = 'WIRIS Plugin formulas';
+$string['connectionsettings'] = 'Connection settings';
+$string['connectionsettings_text'] = '';
+$string['clearcache'] = 'Clear cache';
+$string['clearcachedesc'] = 'Clear wiris filter cache';
+$string['clickwirisplugincorrectlyinstalled'] = 'Click the following button to test if the WIRIS plugin is correctly installed.';
+$string['clickwirisquizzescorrectlyinstalled'] = 'Click the following button to test if the WIRIS quizzes is correctly installed.';
+$string['contact'] = 'For more information or if you have any doubt contact WIRIS Support:';
+$string['editormodalwindow'] = 'Modal window enabled';
+$string['editormodalwindow_help'] = '';
+$string['editormodalwindowfullscreen'] = 'Modal window in full-screen mode';
+$string['editormodalwindowfullscreen_help'] = '';
+$string['editornameexpected'] = 'Editor name is expected';
+$string['enabled'] = 'Enabled';
+$string['disabled'] = 'Disabled';
+$string['editorsettings'] = 'Editor settings';
+$string['editorsettings_text'] = '';
+$string['error'] = 'ERROR';
+$string['errordeletingcache'] = 'Error deleting cache on {$a} area';
+$string['errorsavingcache'] = 'Error saving cache on {$a} area';
+$string['filtername'] = 'Math & Science by WIRIS';
+$string['filter_standalone'] = 'WIRIS filter only';
+$string['filter_standalonedesc'] = 'You will be able to display existing formulas but creation of new formulas is not available. It also removes the warning message.';
+$string['filtermustbeinstalled'] = 'Filter must be installed in Moodle filter folder.';
+$string['furtherinformation'] = 'For further information see our documentation page ';
+$string['havesameversion'] = ' have the same version';
+$string['imageformat'] = 'Image format';
+$string['imageformat_help'] = '';
+$string['imageservicehost'] = 'Service host';
+$string['imageservicehost_help'] = '';
+$string['imageservicepath'] = 'Service path';
+$string['imageservicepath_help'] = '';
+$string['imageserviceport'] = 'Service port';
+$string['imageserviceport_help'] = '';
+$string['imageserviceprotocol'] = 'Service protocol';
+$string['imageserviceprotocol_help'] = '';
+$string['imagesettings'] = 'Image settings';
+$string['imagesettings_text'] = '';
+$string['impossibletofindwirisfilterversion'] = 'Impossible to find WIRIS plugin filter version file.';
+$string['lookingforcorrectfolder'] = 'Looking for correct folder';
+$string['lookingforfilterfiles'] = 'Looking for filter files';
+$string['lookingforwirisfilterversion'] = 'Looking for WIRIS plugin filter version';
+$string['lookingforwirisplugin'] = 'Looking for WIRIS plugin for ';
+$string['lookingforwirispluginenabled'] = 'Looking for WIRIS plugin enabled for  ';
+$string['mustbeinstalled'] = ' must be installed.';
+$string['noteditorspluginsinstalled'] = 'There are not editors plugins installed';
+$string['notsupportededitor'] = ' is not a supported editor.';
+$string['ok'] = 'OK';
+$string['oldconfiguration'] = 'An old configuration.ini file has been detected on {$a}. Please move it to /filter/wiris/configuration.ini.';
+$string['onlytextareaeditorinstalled'] = 'There are only a textarea editors plugin installed';
+$string['or'] = 'or ';
+$string['pluginname'] = 'WIRIS filter';
+$string['pluginperformance'] = 'Image performance mode';
+$string['pluginperformance_help'] = '';
+$string['securitysettings'] = 'Security settings';
+$string['securitysettings_text'] = '';
+$string['title'] = 'Moodle 2.x WIRIS plugin filter test page';
+$string['tinymce'] = 'TinyMCE';
+$string['uninstallmode'] = 'Uninstall mode';
+$string['uninstallmodedesc'] = 'Allows WIRIS plugin to be uninstalled';
+$string['version'] = 'version';
+$string['versionsdontmatch'] = ' versions don\'t match';
+$string['windowsettings'] = 'Window settings';
+$string['windowsettings_text'] = '';
+$string['wirisbehaterroreditornotfound'] = 'Could not find wiris editor';
+$string['wirisfilterversion'] = 'WIRIS Filter version = ';
+$string['wirispluginfilterfor'] = 'WIRIS plugin filter and WIRIS plugin for ';
+$string['wirispluginfor'] = 'WIRIS plugin for ';
+$string['wirispluginforatto'] = 'WIRIS PLUGIN for Atto ';
+$string['wirispluginfortinymce'] = 'WIRIS PLUGIN for TinyMCE ';
+$string['wirismatheditor'] = 'Math editor';
+$string['wirispluginfiltermustbeinstalled'] = 'WIRIS plugin filter for Moodle 2.x must be installed.';
+$string['wirischemeditor'] = 'Chemistry editor';
+$string['wiriscas'] = 'Calculator';
