04b27c1b9b3d7bcc51d9bd481ea2cd2396814559
Ort Moodle developer <MoodleDev@ort.org.il>
2019-11-03 09:37:52 +0200

    theme/aviv2018/ (update) adds search functionality in question categories select box

---
+++ theme/aviv2018/javascript/applyselect2.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+$(document).ready(function () {
+    if ($('#id_selectacategory').length > 0 && $.fn.select2) {
+        $('#id_selectacategory').select2();
+    }
+});
