8c8e7fae013ca349e61644ceb464960dcf91e848
Ort Moodle developer <MoodleDev@ort.org.il>
2020-07-26 12:54:09 +0300

    blocks/anonymous_user/lang/ - fix the super-guest's schoool name

--- blocks/anonymous_user/lang/en/block_anonymous_user.php
+++ blocks/anonymous_user/lang/en/block_anonymous_user.php
@@ -6,5 +6,5 @@ $string['pluginname'] = 'Generate Anonymous Users';
 $string['super'] = 'Super';
 $string['guest'] = 'Guest';
 $string['city'] = 'Ramat Gan';
-$string['institution'] = 'Mop School';
+$string['institution'] = 'Super Guest School';
 $string['aviv_courses'] = '632,454,678,679,911,437,939,932,688,689,266,918,934,686,685,684,683';
--- blocks/anonymous_user/lang/he/block_anonymous_user.php
+++ blocks/anonymous_user/lang/he/block_anonymous_user.php
@@ -6,5 +6,5 @@ $string['pluginname'] = 'יצירת משתמשים זמניים';
 $string['super'] = 'משתמש';
 $string['guest'] = 'אורח';
 $string['city'] = 'רמת גן';
-$string['institution'] = 'בי"ס מו"פ';
+$string['institution'] = 'בי"ס משתמש אורח';
 $string['aviv_courses'] = '632,454,678,679,911,437,939,932,688,689,266,918,934,686,685,684,683,6756';
